a8fcb8c61aed33ebc5d8fce19a09cb6e8a1ae323
lihiniapsara <apsaralihini11@gmail.com>
2024-10-31 13:37:41 +0530

    pos done

---
+++ .idea/model/OrderModel.js
@@ -0,0 +1,48 @@
+export default  class OrderModel {
+    constructor(o_id,c_id, i_code, quantity, total) {
+        this._o_id = o_id;
+        this._c_id = c_id;
+        this._i_code = i_code;
+        this._quantity = quantity;
+        this._total = total;
+    }
+
+    get o_id() {
+        return this._o_id;
+    }
+    set o_id(value) {
+        this._o_id = value;
+    }
+
+    get c_id() {
+        return this._c_id;
+    }
+
+    set c_id(value) {
+        this._c_id = value;
+    }
+
+    get i_code() {
+        return this._i_code;
+    }
+
+    set i_code(value) {
+        this._i_code = value;
+    }
+
+    get quantity() {
+        return this._quantity;
+    }
+
+    set quantity(value) {
+        this._quantity = value;
+    }
+
+    get total() {
+        return this._total;
+    }
+
+    set total(value) {
+        this._total = value;
+    }
+}
